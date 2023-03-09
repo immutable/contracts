897f89d935cfee5dd74915b151e9e6f73b7ff3bb
JamesSnewin <james.snewin@immutable.com>
2023-03-09 13:12:15 +1000

    address changes

--- contracts/token/erc721/ImmutableERC721Base.sol
+++ contracts/token/erc721/ImmutableERC721Base.sol
@@ -13,7 +13,13 @@ import "../../access/IERC173.sol";
 // Utils
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ImmutableERC721Base is
+/*
+    ImmutableERC721Base is an abstract contract that offers minimum preset functionality without
+    an opinionated form of minting. This contract is intended to be inherited and have the `mint`
+    function overridden to meet the needs of the inheriting contract.
+*/
+
+abstract contract ImmutableERC721Base is
     ERC721,
     ERC721Enumerable,
     ERC721Burnable,
@@ -112,6 +118,9 @@ contract ImmutableERC721Base is
         contractURI = _contractURI;
     }
 
+    //@dev mint function to be overriden by inheriting contracts
+    function mint(address to, uint256 amount) external virtual{}
+
     /// @dev Allows admin to update contract owner. Required that new oner has admin role
     function transferOwnership(address newOwner)
         external
@@ -122,11 +131,11 @@ contract ImmutableERC721Base is
             hasRole(DEFAULT_ADMIN_ROLE, newOwner),
             "New owner does not have default admin role"
         );
-        require(_owner != newOwner, "New owner is currently owner");
-        require(msg.sender == _owner, "Caller must be current owner");
-        address oldOwner = _owner;
+        address owner_ = _owner;
+        require(owner_ != newOwner, "New owner is currently owner");
+        require(msg.sender == owner_, "Caller must be current owner");
         _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+        emit OwnershipTransferred(owner_, newOwner);
     }
 
     /// @dev Internal hook implemented in {ERC721Enumerable}, required for totalSupply()

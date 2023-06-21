3646e187eaa508528bb5083c8bc9e3a7be005092
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2023-06-21 13:30:28 +1000

    Fix eslint config issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,18 @@ module.exports = {
     browser: false,
     es2021: true,
     mocha: true,
-    node: true,
+    node: true
   },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 12
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
-  },
-};
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] }
+    ]
+  }
+}

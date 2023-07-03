af89344c382fd04ab773858d48b54021e4cb7a5c
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2023-07-03 15:45:02 +1000

    Update AllowlistERC721TransfersApprovals.test.ts

--- test/royalty-enforcement/AllowlistERC721TransfersApprovals.test.ts
+++ test/royalty-enforcement/AllowlistERC721TransfersApprovals.test.ts
@@ -120,17 +120,6 @@ describe("Allowlisted ERC721 Transfers", function () {
       // TODO: REZ
     });
 
-    it("Should not allow a non-Allowlisted operator to approve", async function () {
-      await erc721.connect(minter).mint(minter.address, 3);
-      // Approve EOA addr
-      await erc721.connect(minter).approve(accs[0].address, 1);
-      await erc721.connect(minter).setApprovalForAll(accs[0].address, true);
-      expect(await erc721.getApproved(1)).to.be.equal(accs[0].address);
-      expect(
-        await erc721.isApprovedForAll(minter.address, accs[0].address)
-      ).to.be.equal(true);
-    });
-
     it("Should allow EOAs to be approved", async function () {
       await erc721.connect(minter).mint(minter.address, 3);
       // Approve EOA addr

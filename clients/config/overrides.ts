e2f4c475b0670f94d97b304e4de8001bc81a1073
Allan Almeida <allan.almeida@immutable.com>
2023-12-01 10:50:18 +1100

    add default gas overrides

---
+++ clients/config/overrides.ts
@@ -0,0 +1,7 @@
+import { CallOverrides } from "@ethersproject/contracts";
+
+export const defaultGasOverrides: CallOverrides = {
+  maxFeePerGas: 1000000000000,
+  maxPriorityFeePerGas: 1000000000000,
+  gasLimit: 20000000,
+};

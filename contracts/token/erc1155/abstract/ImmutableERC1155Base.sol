c5054a7ea4a3d2cd5401d9f320d9e89732e94d90
jasonzwli <134979033+jasonzwli@users.noreply.github.com>
2023-12-06 10:11:11 +1100

    [gms-1116][chore] tests for 1155, amend baseuri (#144)
    * [gms-1116][chore] tests for 1155, amend baseuri
    
    * remove submodule config from checkout action
    
    * print files
    
    * check if all mock files are broken
    
    * fix typo
    
    * undo github action change
    
    ---------
    
    Co-authored-by: Ipek Turgul <ipek.turgul@immutable.com>

--- contracts/token/erc1155/abstract/ImmutableERC1155Base.sol
+++ contracts/token/erc1155/abstract/ImmutableERC1155Base.sol
@@ -19,10 +19,7 @@ abstract contract ImmutableERC1155Base is
     string public contractURI;
 
     /// @dev Common URIs for individual token URIs
-    string public baseURI;
-
-    // Optional mapping for token URIs
-    mapping(uint256 => string) private _tokenURIs;
+    string private _baseURI;
 
     /// @dev Only MINTER_ROLE can invoke permissioned mint.
     bytes32 public constant MINTER_ROLE = bytes32("MINTER_ROLE");
@@ -52,165 +49,7 @@ abstract contract ImmutableERC1155Base is
         _setDefaultRoyalty(_receiver, _feeNumerator);
         _setOperatorAllowlistRegistry(_operatorAllowlist);
         contractURI = contractURI_;
-        baseURI = baseURI_;
-    }
-
-    /**
-     * @notice Total value of tokens in with a given id.
-     * @param id The token identifier to retrieve the total supply for.
-     */
-    function totalSupply(uint256 id) public view virtual returns (uint256) {
-        return _totalSupply[id];
-    }
-
-    /**
-     * @notice Indicates whether any token exist with a given id, or not.
-     * @param id The token identifier to check for existence.
-     */
-    function exists(uint256 id) public view virtual returns (bool) {
-        return totalSupply(id) > 0;
-    }
-
-    /**
-     * @notice
-     *
-     * This implementation returns the concatenation of the `_baseURI`
-     * and the token-specific uri if the latter is set
-     *
-     * This enables the following behaviors:
-     *
-     * - if `_tokenURIs[tokenId]` is set, then the result is the concatenation
-     *   of `super.uri()` and `_tokenURIs[tokenId]`
-     *
-     * - if `_tokenURIs[tokenId]` is NOT set then we fallback to `super.uri()`
-     *   which in most cases will contain `ERC1155._uri`;
-     *
-     * - if `_tokenURIs[tokenId]` is NOT set, and if the parents do not have a
-     *   uri value set, then the result is empty.
-     */
-    function uri(uint256 tokenId) public view virtual override returns (string memory) {
-        string memory tokenURI = _tokenURIs[tokenId];
-
-        // If token URI is set, concatenate base URI and tokenURI (via string.concat).
-        return bytes(tokenURI).length > 0 ? string.concat(super.uri(tokenId), tokenURI) : super.uri(tokenId);
-    }
-
-    /**
-     * @notice Sets `tokenURI` as the tokenURI of `tokenId`.
-     * @param tokenId The token identifier to set the URI for.
-     * @param tokenURI The URI to assign.
-     */
-    function setTokenURI(uint256 tokenId, string memory tokenURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _tokenURIs[tokenId] = tokenURI;
-    }
-
-    /**
-     * @notice See Openzepplin ERC1155._beforeTokenTransfer.
-     */
-    function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal virtual override {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-
-        if (from == address(0)) {
-            for (uint256 i = 0; i < ids.length; ++i) {
-                _totalSupply[ids[i]] += amounts[i];
-            }
-        }
-
-        if (to == address(0)) {
-            for (uint256 i = 0; i < ids.length; ++i) {
-                uint256 id = ids[i];
-                uint256 amount = amounts[i];
-                uint256 supply = _totalSupply[id];
-                require(supply >= amount, "ERC1155: burn amount exceeds totalSupply");
-                unchecked {
-                    _totalSupply[id] = supply - amount;
-                }
-            }
-        }
-    }
-
-    /**
-     * @notice Sets `_baseURI` as the `_baseURI` for all tokens
-     * @param baseURI_ The base URI for all tokens
-     */
-    function setBaseURI(string memory baseURI_) public onlyRole(DEFAULT_ADMIN_ROLE) {
-       _setURI(baseURI_);
-       baseURI = baseURI_;
-    }
-
-    /// @dev Allows admin to set the contract URI
-    function setContractURI(string memory contractURI_) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        contractURI = contractURI_;
-    }
-
-    /**
-     * @notice Overrides supportsInterface from ERC1155, ERC2981, and OperatorAllowlistEnforced
-     * @param interfaceId The interface identifier, which is a 4-byte selector.
-     * @return True if the contract implements `interfaceId` and the call doesn't revert, otherwise false.
-     */
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        virtual
-        override(ERC1155Permit, ERC2981, OperatorAllowlistEnforced)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
-    /**
-     * @notice Returns the addresses which have DEFAULT_ADMIN_ROLE
-     * @return admins The addresses which have DEFAULT_ADMIN_ROLE
-     */
-    function getAdmins() public view returns (address[] memory) {
-        uint256 adminCount = getRoleMemberCount(DEFAULT_ADMIN_ROLE);
-        address[] memory admins = new address[](adminCount);
-        for (uint256 i; i < adminCount; i++) {
-            admins[i] = getRoleMember(DEFAULT_ADMIN_ROLE, i);
-        }
-        return admins;
-    }
-
-    /**
-     * @notice Override of setApprovalForAll from {ERC721}, with added Allowlist approval validation
-     * @param operator The address to approve as an operator for the caller.
-     * @param approved True if the operator is approved, false to revoke approval.
-     */
-    function setApprovalForAll(address operator, bool approved) public override(ERC1155) validateApproval(operator) {
-        super.setApprovalForAll(operator, approved);
-    }
-
-    /**
-     * @notice Override of _safeTransferFrom from {ERC1155}, with added Allowlist transfer validation
-     * @param from The current owner of the token.
-     * @param to The new owner.
-     * @param id The token identifier to transfer.
-     * @param value The amount to transfer.
-     * @param data Additional data with no specified format, sent in call to `to`.
-     */
-    function _safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data) internal override validateTransfer(from, to) {
-        super._safeTransferFrom(from, to, id, value, data);
-    }
-
-    /**
-     * @notice Override of _safeBatchTransferFrom from {ERC1155}, with added Allowlist transfer validation
-     * @param from The current owner of the token.
-     * @param to The new owner.
-     * @param ids The token identifiers to transfer.
-     * @param values The amounts to transfer per token id.
-     * @param data Additional data with no specified format, sent in call to `to`.
-     */
-    function _safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory values, bytes memory data) internal override validateTransfer(from, to) {
-        super._safeBatchTransferFrom(from, to, ids, values, data);
+        _baseURI = baseURI_;
     }
 
     /**
@@ -267,4 +106,169 @@ abstract contract ImmutableERC1155Base is
     function revokeMinterRole(address user) public onlyRole(DEFAULT_ADMIN_ROLE) {
         revokeRole(MINTER_ROLE, user);
     }
+    
+    /**
+     * @notice Override of setApprovalForAll from {ERC721}, with added Allowlist approval validation
+     * @param operator The address to approve as an operator for the caller.
+     * @param approved True if the operator is approved, false to revoke approval.
+     */
+    function setApprovalForAll(address operator, bool approved) public override(ERC1155) validateApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    /**
+     * @notice Sets `_baseURI` as the `_baseURI` for all tokens
+     * @param baseURI_ The base URI for all tokens
+     */
+    function setBaseURI(string memory baseURI_) public onlyRole(DEFAULT_ADMIN_ROLE) {
+       _setURI(baseURI_);
+       _baseURI = baseURI_;
+    }
+
+    /// @dev Allows admin to set the contract URI
+    function setContractURI(string memory contractURI_) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        contractURI = contractURI_;
+    }
+
+    /**
+     * @notice Total value of tokens in with a given id.
+     * @param id The token identifier to retrieve the total supply for.
+     */
+    function totalSupply(uint256 id) public view virtual returns (uint256) {
+        return _totalSupply[id];
+    }
+
+    /**
+     * @notice Indicates whether any token exist with a given id, or not.
+     * @param id The token identifier to check for existence.
+     */
+    function exists(uint256 id) public view virtual returns (bool) {
+        return totalSupply(id) > 0;
+    }
+
+    /**
+     * @notice Overrides supportsInterface from ERC1155, ERC2981, and OperatorAllowlistEnforced
+     * @param interfaceId The interface identifier, which is a 4-byte selector.
+     * @return True if the contract implements `interfaceId` and the call doesn't revert, otherwise false.
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(ERC1155Permit, ERC2981, OperatorAllowlistEnforced)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @notice See {IERC1155MetadataURI-uri}.
+     *
+     * This implementation returns the same URI for *all* token types. It relies
+     * on the token type ID substitution mechanism
+     * https://eips.ethereum.org/EIPS/eip-1155#metadata[defined in the EIP].
+     *
+     * Clients calling this function must replace the `\{id\}` substring with the
+     * actual token type ID if the uri contains `\{id\}`.
+     */
+    function baseURI() public view virtual returns (string memory) {
+        return _baseURI;
+    }
+    
+    /**
+     * @notice See {IERC1155MetadataURI-uri}.
+     *
+     * This implementation returns the same URI for *all* token types. It relies
+     * on the token type ID substitution mechanism
+     * https://eips.ethereum.org/EIPS/eip-1155#metadata[defined in the EIP].
+     *
+     * Clients calling this function must replace the `\{id\}` substring with the
+     * actual token type ID if the uri contains `\{id\}`.
+     *
+     * returns the baseURI of the contract. This is the same as baseURI()
+     * This is done to unify the uri and baseuri variables for Immutable's metadata
+     * indexing
+     */
+    function uri(uint256) public view virtual override returns (string memory) {
+        return _baseURI;
+    }
+
+    /**
+     * @notice Returns the addresses which have DEFAULT_ADMIN_ROLE
+     * @return admins The addresses which have DEFAULT_ADMIN_ROLE
+     */
+    function getAdmins() public view returns (address[] memory) {
+        uint256 adminCount = getRoleMemberCount(DEFAULT_ADMIN_ROLE);
+        address[] memory admins = new address[](adminCount);
+        for (uint256 i; i < adminCount; i++) {
+            admins[i] = getRoleMember(DEFAULT_ADMIN_ROLE, i);
+        }
+        return admins;
+    }
+
+    /**
+     * @notice See Openzepplin ERC1155._beforeTokenTransfer.
+     * @param operator The address performing the transfer.
+     * @param from The address from which the token is being transferred.
+     * @param to The address to which the token is being transferred.
+     * @param ids The token identifiers to transfer.
+     * @param amounts The amounts to transfer per token id.
+     * @param data Additional data with no specified format, sent in call to `to`.
+     */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+        if (from == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                _totalSupply[ids[i]] += amounts[i];
+            }
+        }
+
+        if (to == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                uint256 id = ids[i];
+                uint256 amount = amounts[i];
+                uint256 supply = _totalSupply[id];
+                require(supply >= amount, "ERC1155: burn amount exceeds totalSupply");
+                unchecked {
+                    _totalSupply[id] = supply - amount;
+                }
+            }
+        }
+    }
+
+    /**
+     * @notice Override of _safeTransferFrom from {ERC1155}, with added Allowlist transfer validation
+     * @param from The current owner of the token.
+     * @param to The new owner.
+     * @param id The token identifier to transfer.
+     * @param value The amount to transfer.
+     * @param data Additional data with no specified format, sent in call to `to`.
+     */
+    function _safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data) internal override validateTransfer(from, to) {
+        super._safeTransferFrom(from, to, id, value, data);
+    }
+
+    /**
+     * @notice Override of _safeBatchTransferFrom from {ERC1155}, with added Allowlist transfer validation
+     * @param from The current owner of the token.
+     * @param to The new owner.
+     * @param ids The token identifiers to transfer.
+     * @param values The amounts to transfer per token id.
+     * @param data Additional data with no specified format, sent in call to `to`.
+     */
+    function _safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory values, bytes memory data) internal override validateTransfer(from, to) {
+        super._safeBatchTransferFrom(from, to, ids, values, data);
+    }
+
+    
 }
--- test/token/erc1155/ImmutableERC1155.t.sol
+++ test/token/erc1155/ImmutableERC1155.t.sol
@@ -4,24 +4,43 @@ pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 import {ImmutableERC1155} from "../../../contracts/token/erc1155/preset/draft-ImmutableERC1155.sol";
 import {IImmutableERC1155Errors} from "../../../contracts/errors/Errors.sol";
+import {OperatorAllowlistEnforcementErrors} from "../../../contracts/errors/Errors.sol";
 import {OperatorAllowlist} from "../../../contracts/allowlist/OperatorAllowlist.sol";
 import {Sign} from "../../utils/Sign.sol";
+import {MockWallet} from "../../../contracts/mocks/MockWallet.sol";
+import {MockWalletFactory} from "../../../contracts/mocks/MockWalletFactory.sol";
 
 contract ImmutableERC1155Test is Test {
     ImmutableERC1155 public immutableERC1155;
     Sign public sign;
     OperatorAllowlist public operatorAllowlist;
+    MockWalletFactory public scmf;
+    MockWallet public mockWalletModule;
+    MockWallet public scw;
+    MockWallet public anotherScw;
+    address[] private operatorAddrs;
+
 
     uint256 deployerPrivateKey = 1;
     uint256 ownerPrivateKey = 2;
     uint256 spenderPrivateKey = 3;
     uint256 anotherPrivateKey = 4;
     uint256 feeReceiverKey = 5;
+    uint256 minterPrivateKey = 6;
+    uint256 scwPrivateKey = 7;
 
     address deployer = vm.addr(deployerPrivateKey);
     address owner = vm.addr(ownerPrivateKey);
     address spender = vm.addr(spenderPrivateKey);
     address feeReceiver = vm.addr(feeReceiverKey);
+    address minter = vm.addr(minterPrivateKey);
+    address scwOwner = vm.addr(scwPrivateKey);
+
+    bytes32 salt = keccak256(abi.encodePacked("0x1234"));
+    bytes32 anotherSalt = keccak256(abi.encodePacked("0x12345"));
+
+    address public scwAddress;
+    address public anotherScwAddress;
 
     function setUp() public {
         operatorAllowlist = new OperatorAllowlist(
@@ -36,7 +55,30 @@ contract ImmutableERC1155Test is Test {
             feeReceiver,
             0
         );
+
+        operatorAddrs.push(minter);
+        vm.startPrank(owner);
+        bytes32 regiRole = operatorAllowlist.REGISTRAR_ROLE();
+        operatorAllowlist.grantRegistrarRole(owner);
+        assertTrue(operatorAllowlist.hasRole(regiRole, owner));
+        vm.stopPrank();
+
         sign = new Sign(immutableERC1155.DOMAIN_SEPARATOR());
+        vm.prank(owner);
+        immutableERC1155.grantMinterRole(minter);
+
+        scmf = new MockWalletFactory();
+
+        vm.prank(scwOwner);
+        mockWalletModule = new MockWallet();
+        scmf.deploy(address(mockWalletModule), salt);
+        scwAddress = scmf.getAddress(address(mockWalletModule), salt);
+        scw = MockWallet(scwAddress);
+
+        mockWalletModule = new MockWallet();
+        scmf.deploy(address(mockWalletModule), anotherSalt);
+        anotherScwAddress = scmf.getAddress(address(mockWalletModule), anotherSalt);
+        anotherScw = MockWallet(anotherScwAddress);
     }
 
     function _sign(
@@ -54,6 +96,30 @@ contract ImmutableERC1155Test is Test {
         sig = abi.encodePacked(r, s, v);
     }
 
+    function _addAddrToAllowListAndApprove() private {
+        vm.startPrank(owner);
+        operatorAllowlist.addAddressToAllowlist(operatorAddrs);
+        immutableERC1155.setApprovalForAll(minter, true);
+        vm.stopPrank();
+    }
+
+    function _addSCWAddressAllowListAndApprove(address _address) private {
+        vm.startPrank(owner);
+        operatorAllowlist.addWalletToAllowlist(_address);
+        immutableERC1155.setApprovalForAll(_address, true);
+        vm.stopPrank();
+    }
+
+    /*
+    * Role
+    */
+    function test_giveMinterRole() public {
+        vm.prank(owner);
+        immutableERC1155.grantMinterRole(minter);
+        bytes32 minterRole = immutableERC1155.MINTER_ROLE();
+        assertTrue(immutableERC1155.hasRole(minterRole, minter));
+    }
+
     /*
     * Contract deployment
     */
@@ -68,6 +134,11 @@ contract ImmutableERC1155Test is Test {
 
     function test_DeploymentShouldSetBaseURI() public {
         assertEq(immutableERC1155.baseURI(), "test-base-uri");
+    }
+
+    function test_DeploymentAllowlistShouldGiveAdminToOwner() public {
+        bytes32 adminRole = operatorAllowlist.DEFAULT_ADMIN_ROLE();
+        assertTrue(operatorAllowlist.hasRole(adminRole, owner));
     }
 
     /*
@@ -133,5 +204,174 @@ contract ImmutableERC1155Test is Test {
         vm.expectRevert(IImmutableERC1155Errors.PermitExpired.selector);
 
         immutableERC1155.permit(owner, spender, true, 1 days, sig);
+    }
+
+    /*
+    * Mints
+    */
+    function test_MinterRoleCanMint() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(minter, 1, 1, "");
+        assertEq(immutableERC1155.balanceOf(minter, 1), 1);
+    }
+
+    function test_MinterRoleCanBatchMint() public {
+        vm.prank(minter);
+        uint256[] memory ids = new uint256[](3);
+        uint256[] memory amounts = new uint256[](3);
+
+        ids[0] = 1;
+        ids[1] = 2;
+        ids[2] = 3;
+
+        amounts[0] = 10;
+        amounts[1] = 5;
+        amounts[2] = 9;
+        immutableERC1155.safeMintBatch(minter, ids, amounts, "");
+        assertEq(immutableERC1155.balanceOf(minter, 1), 10);
+        assertEq(immutableERC1155.balanceOf(minter, 2), 5);
+        assertEq(immutableERC1155.balanceOf(minter, 3), 9);
+    }
+
+    /*
+    * transfers
+    */
+    function test_ApprovedOperatorTransferFrom() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+
+        _addAddrToAllowListAndApprove();
+
+        vm.prank(minter);
+        immutableERC1155.safeTransferFrom(owner, spender, 1, 1, "");
+        assertEq(immutableERC1155.balanceOf(spender, 1), 1);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 0);
+    }
+
+    function test_ApprovedOperatorBatchTransferFrom() public {
+        vm.startPrank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+        immutableERC1155.safeMint(owner, 2, 1, "");
+        vm.stopPrank();
+
+        _addAddrToAllowListAndApprove();
+
+        vm.prank(minter);
+        uint256[] memory ids = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
+
+        ids[0] = 1;
+        ids[1] = 2;
+
+        amounts[0] = 1;
+        amounts[1] = 1;
+        immutableERC1155.safeBatchTransferFrom(owner, spender, ids, amounts, "");
+        assertEq(immutableERC1155.balanceOf(spender, 1), 1);
+        assertEq(immutableERC1155.balanceOf(spender, 2), 1);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 0);
+        assertEq(immutableERC1155.balanceOf(owner, 2), 0);
+    }
+
+    function test_ApprovedSCWOperatorTransferFrom() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+
+        _addSCWAddressAllowListAndApprove(scwAddress);
+
+        vm.prank(scwOwner);
+        scw.transfer1155(address(immutableERC1155), owner, spender, 1, 1);
+        assertEq(immutableERC1155.balanceOf(spender, 1), 1);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 0);
+    }
+
+    function test_ApprovedSCWOperatorTransferFromToApprovedReceiver() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+
+        _addSCWAddressAllowListAndApprove(scwAddress);
+
+        vm.prank(scwOwner);
+        scw.transfer1155(address(immutableERC1155), owner, scwAddress, 1, 1);
+        assertEq(immutableERC1155.balanceOf(scwAddress, 1), 1);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 0);
+    }
+
+    function test_ApprovedSCWOperatorTransferFromToUnApprovedReceiver() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+
+        _addSCWAddressAllowListAndApprove(scwAddress);
+
+        vm.expectRevert(abi.encodeWithSignature("TransferToNotInAllowlist(address)", anotherScwAddress));
+
+        vm.prank(scwOwner);
+        scw.transfer1155(address(immutableERC1155), owner, anotherScwAddress, 1, 1);
+    }
+
+    function test_ApprovedSCWOperatorBatchTransferFromToApprovedReceiver() public {
+        vm.startPrank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+        immutableERC1155.safeMint(owner, 2, 1, "");
+        vm.stopPrank();
+
+        _addSCWAddressAllowListAndApprove(scwAddress);
+
+        vm.prank(scwOwner);
+        uint256[] memory ids = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
+
+        ids[0] = 1;
+        ids[1] = 2;
+
+        amounts[0] = 1;
+        amounts[1] = 1;
+        scw.batchTransfer1155(address(immutableERC1155), owner, scwAddress, ids, amounts);
+        assertEq(immutableERC1155.balanceOf(scwAddress, 1), 1);
+        assertEq(immutableERC1155.balanceOf(scwAddress, 2), 1);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 0);
+        assertEq(immutableERC1155.balanceOf(owner, 2), 0);
+    }
+
+    /*
+    * Approve
+    */
+    function test_UnapprovedSCWOperatorTransferFrom() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+        vm.expectRevert(abi.encodeWithSignature("ApproveTargetNotInAllowlist(address)", scwAddress));
+        vm.startPrank(owner);
+        immutableERC1155.setApprovalForAll(scwAddress, true);
+        vm.stopPrank();
+    }
+
+   /*
+   * Burn
+   */
+   function test_Burn() public {
+        vm.prank(minter);
+        immutableERC1155.safeMint(owner, 1, 1, "");
+        vm.prank(owner);
+        immutableERC1155.burn(owner, 1, 1);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 0);
+    }
+
+    function test_BatchBurn() public {
+        vm.startPrank(minter);
+        immutableERC1155.safeMint(owner, 1, 10, "");
+        immutableERC1155.safeMint(owner, 2, 10, "");
+        assertEq(immutableERC1155.totalSupply(1), 10);
+        vm.stopPrank();
+
+        vm.prank(owner);
+        uint256[] memory ids = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
+        ids[0] = 1;
+        ids[1] = 2;
+        amounts[0] = 5;
+        amounts[1] = 3;
+        immutableERC1155.burnBatch(owner, ids, amounts);
+        assertEq(immutableERC1155.balanceOf(owner, 1), 5);
+        assertEq(immutableERC1155.balanceOf(owner, 2), 7);
+        assertEq(immutableERC1155.totalSupply(1), 5);
     }
 }

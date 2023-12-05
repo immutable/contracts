b3c7d79f6479b21bd2040e24f7b6798d0b48c6a4
jason li <jason.li@immutable.com>
2023-12-05 14:28:19 +1100

    [gms-1116][chore] tests for 1155, amend baseuri

--- contracts/token/erc1155/preset/draft-ImmutableERC1155.sol
+++ contracts/token/erc1155/preset/draft-ImmutableERC1155.sol
@@ -39,8 +39,7 @@ contract ImmutableERC1155 is ImmutableERC1155Base {
         super._mint(to, id, value, data);
     }
 
-    function safeMintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) external onlyRole(MINTER_ROLE) {
+    function safeMintBatch(address to, uint256[] calldata ids, uint256[] calldata values, bytes memory data) external onlyRole(MINTER_ROLE) {
         super._mintBatch(to, ids, values, data);
     }
-
 }

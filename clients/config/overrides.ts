57e9d0d4f76c1ae86113d6fb29378065a792c6e4
Allan Almeida <allan.almeida@immutable.com>
2023-12-07 10:35:52 +1100

    update config

--- clients/config/overrides.ts
+++ clients/config/overrides.ts
@@ -2,6 +2,6 @@ import { CallOverrides } from "@ethersproject/contracts";
 
 // https://docs.immutable.com/docs/zkEVM/architecture/gas-config
 export const defaultGasOverrides: CallOverrides = {
-  maxPriorityFeePerGas: 100e9, // 100 Gwei
-  maxFeePerGas: 150e9,
+  maxPriorityFeePerGas: 10e9, // 10 Gwei
+  maxFeePerGas: 15e9,
 };

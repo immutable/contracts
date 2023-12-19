015c4f2dcd77790c800bf8f663bb362184208778
Peter Robinson <drinkcoffee@eml.cc>
2023-12-19 14:26:57 +1000

    Temporarily remove trading contracts

--- contracts/trading/seaport/validators/ReadOnlyOrderValidator.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-import { ReadOnlyOrderValidator } from "seaport/contracts/helpers/order-validator/lib/ReadOnlyOrderValidator.sol";
--- contracts/trading/seaport/validators/SeaportValidator.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-import { SeaportValidator } from "seaport/contracts/helpers/order-validator/SeaportValidator.sol";

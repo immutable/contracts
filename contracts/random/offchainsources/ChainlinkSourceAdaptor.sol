26b499155d026af707746e83bc1bad2eea12aac1
Peter Robinson <drinkcoffee@eml.cc>
2024-01-15 11:27:38 +1000

    Clean compile on hardhat and forge.

--- contracts/random/offchainsources/ChainlinkSourceAdaptor.sol
+++ contracts/random/offchainsources/ChainlinkSourceAdaptor.sol
@@ -3,8 +3,8 @@
 pragma solidity 0.8.19;
 
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import "chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "./VRFCoordinatorV2Interface.sol";
 import "../IOffchainRandomSource.sol";
 
 /**

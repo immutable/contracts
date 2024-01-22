f70d09315af9b5c46c7983e91125a98aa3dcf080
Peter Robinson <drinkcoffee@eml.cc>
2024-01-23 09:23:55 +1000

    Remove chainlink package and instead include just the file needed

--- contracts/random/offchainsources/chainlink/ChainlinkSourceAdaptor.sol
+++ contracts/random/offchainsources/chainlink/ChainlinkSourceAdaptor.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache 2
 pragma solidity 0.8.19;
 
-import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import {VRFConsumerBaseV2} from "./VRFConsumerBaseV2.sol";
 import {VRFCoordinatorV2Interface} from "./VRFCoordinatorV2Interface.sol";
 import {SourceAdaptorBase} from "../SourceAdaptorBase.sol";
 import {IOffchainRandomSource} from "../IOffchainRandomSource.sol";

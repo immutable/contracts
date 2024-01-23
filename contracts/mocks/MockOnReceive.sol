0f670079a359944454224277b46688d343ec704d
Peter Robinson <drinkcoffee@eml.cc>
2024-01-23 16:31:55 +1000

    Remove warnings

--- contracts/mocks/MockOnReceive.sol
+++ contracts/mocks/MockOnReceive.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
@@ -13,10 +14,10 @@ contract MockOnReceive {
 
     // Attempt to transfer token to another address on receive
     function onERC721Received(
-        address operator,
-        address from,
+        address /* operator */,
+        address /* from */,
         uint256 tokenId,
-        bytes calldata data
+        bytes calldata /* data */
     ) public returns (bytes4) {
         tokenAddress.transferFrom(address(this), recipient, tokenId);
         return this.onERC721Received.selector;

aa17c448bce62126d5154861c6146f5505b7cb57
jason li <jason.li@immutable.com>
2024-01-24 13:51:57 +1100

    [gms-1387] migrate OAL typescript tests to forge

--- test/allowlist/OperatorAllowlistUpgradeable.t.sol
+++ test/allowlist/OperatorAllowlistUpgradeable.t.sol
@@ -8,26 +8,30 @@ import {OperatorAllowlistUpgradeable} from "../../contracts/allowlist/OperatorAl
 import {MockOperatorAllowlistUpgradeable} from "./MockOAL.sol";
 import {ImmutableERC721} from "../../contracts/token/erc721/preset/ImmutableERC721.sol";
 import {DeployOperatorAllowlist} from  "../utils/DeployAllowlistProxy.sol";
+import {DeploySCWallet} from "../utils/DeploySCW.sol";
+import {IWalletProxy} from "../../contracts/allowlist/IWalletProxy.sol";
 
-
-contract OperatorAllowlistTest is Test {
+contract OperatorAllowlistTest is Test, OperatorAllowlistUpgradeable {
     OperatorAllowlistUpgradeable public allowlist;
     ImmutableERC721 public immutableERC721;
     MockOperatorAllowlistUpgradeable public oalV2;
+    DeploySCWallet public deploySCWScript;
 
     uint256 feeReceiverKey = 1;
 
     address public admin = makeAddr("roleAdmin");
     address public upgrader = makeAddr("roleUpgrader");
-    address public registerar = makeAddr("roleRegisterar");
+    address public registrar = makeAddr("roleRegisterar");
+    address public scwOwner = makeAddr("scwOwner");
     address feeReceiver = vm.addr(feeReceiverKey);
     address proxyAddr;
     address nonAuthorizedWallet;
-    
+    address scwAddr;
+    address scwModuleAddr;
 
     function setUp() public {
         DeployOperatorAllowlist deployScript = new DeployOperatorAllowlist();
-        proxyAddr = deployScript.run(admin, upgrader, registerar);
+        proxyAddr = deployScript.run(admin, upgrader, registrar);
 
         allowlist = OperatorAllowlistUpgradeable(proxyAddr);
 
@@ -43,11 +47,13 @@ contract OperatorAllowlistTest is Test {
         );
 
         nonAuthorizedWallet = address(0x2);
+
+        deploySCWScript = new DeploySCWallet();
     }
 
     function testDeployment() public {
         assertTrue(allowlist.hasRole(allowlist.DEFAULT_ADMIN_ROLE(), admin));
-        assertTrue(allowlist.hasRole(allowlist.REGISTRAR_ROLE(), registerar));
+        assertTrue(allowlist.hasRole(allowlist.REGISTRAR_ROLE(), registrar));
         assertTrue(allowlist.hasRole(allowlist.UPGRADE_ROLE(), upgrader));
         assertEq(address(immutableERC721.operatorAllowlist()), proxyAddr);
     }
@@ -69,5 +75,89 @@ contract OperatorAllowlistTest is Test {
         vm.prank(nonAuthorizedWallet);
         vm.expectRevert("Must have upgrade role to upgrade");
         allowlist.upgradeTo(address(oalImplV2));
+    }
+
+    function testShouldSupportIOperatorAllowlistInterface() public {
+        assertTrue(allowlist.supportsInterface(0x05a3b809));
+    }
+
+    function testShouldLimitAllowlistAddAndRemoveFunctionality() public {
+        address[] memory addressTargets = new address[](1);
+        addressTargets[0] = address(0x1);
+        vm.startPrank(admin);
+
+        vm.expectRevert("AccessControl: account 0xe48648ee1c7285ff3ad32fa99c427884666caf17 is missing role 0x5245474953545241525f524f4c45000000000000000000000000000000000000");
+        allowlist.addAddressesToAllowlist(addressTargets);
+
+        vm.expectRevert("AccessControl: account 0xe48648ee1c7285ff3ad32fa99c427884666caf17 is missing role 0x5245474953545241525f524f4c45000000000000000000000000000000000000");
+        allowlist.removeAddressesFromAllowlist(addressTargets);
+
+        vm.expectRevert("AccessControl: account 0xe48648ee1c7285ff3ad32fa99c427884666caf17 is missing role 0x5245474953545241525f524f4c45000000000000000000000000000000000000");
+        allowlist.addWalletToAllowlist(address(0x3));
+
+        vm.expectRevert("AccessControl: account 0xe48648ee1c7285ff3ad32fa99c427884666caf17 is missing role 0x5245474953545241525f524f4c45000000000000000000000000000000000000");
+        allowlist.removeWalletFromAllowlist(address(0x3));
+
+        vm.stopPrank();
+    }
+
+    function testShouldAddAndRemoveSmartContractWalletBytecodeFromAllowlist() public {
+        bytes32 salt = keccak256(abi.encodePacked("0x1234"));
+        (scwAddr, scwModuleAddr) = deploySCWScript.run(salt);
+
+        IWalletProxy proxy = IWalletProxy(scwAddr);
+        address implementationAddress = proxy.PROXY_getImplementation();
+        assertEq(implementationAddress, scwModuleAddr);
+
+        bytes memory deployedBytecode = scwAddr.code;
+
+        vm.startPrank(registrar);
+
+        vm.expectEmit(true, true, true, false, address(allowlist)); 
+        emit WalletAllowlistChanged(keccak256(abi.encodePacked(deployedBytecode)), scwAddr, true);
+        allowlist.addWalletToAllowlist(scwAddr);
+        assertTrue(allowlist.isAllowlisted(scwAddr));
+
+        vm.expectEmit(true, true, true, false, address(allowlist));
+        emit WalletAllowlistChanged(keccak256(abi.encodePacked(deployedBytecode)), scwAddr, false);
+        allowlist.removeWalletFromAllowlist(scwAddr);
+        assertFalse(allowlist.isAllowlisted(scwAddr));
+
+        vm.stopPrank(); 
+    }
+
+    function testShouldAddAndRemoveAnAddressOfAMarketPlaceAndRemoveItFromAllowlist() public {
+        address[] memory addressTargets = new address[](1);
+        addressTargets[0] = address(0x1);
+
+        vm.startPrank(registrar);
+
+        vm.expectEmit(true, true, true, false, address(allowlist));
+        emit AddressAllowlistChanged(addressTargets[0], true);
+        allowlist.addAddressesToAllowlist(addressTargets);
+        assertTrue(allowlist.isAllowlisted(addressTargets[0]));
+
+        vm.expectEmit(true, true, true, false, address(allowlist));
+        emit AddressAllowlistChanged(addressTargets[0], false);
+        allowlist.removeAddressesFromAllowlist(addressTargets);
+        assertFalse(allowlist.isAllowlisted(addressTargets[0]));
+
+        vm.stopPrank();
+    }
+
+    function testShouldNotAllowlistSCWWithSameBytecodeButDifferentImplementationAddress() public {
+        bytes32 salt1 = keccak256(abi.encodePacked("0x5678"));
+        address firstScwAddr;
+        (firstScwAddr, ) = deploySCWScript.run(salt1);
+
+        vm.startPrank(registrar);
+        allowlist.addWalletToAllowlist(firstScwAddr);
+        assertTrue(allowlist.isAllowlisted(firstScwAddr));
+
+        bytes32 salt2 = keccak256(abi.encodePacked("0x5678"));
+        address secondScwAddr;
+        (secondScwAddr, ) = deploySCWScript.run(salt2);
+        assertFalse(allowlist.isAllowlisted(secondScwAddr));
+        vm.stopPrank();
     }
 }

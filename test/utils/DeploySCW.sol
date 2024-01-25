f415b6daceaa37347053405a843c7fae4b3d3b65
jasonzwli <134979033+jasonzwli@users.noreply.github.com>
2024-01-25 11:14:57 +1100

    [gms-1387] migrate OAL typescript tests to forge (#167)

---
+++ test/utils/DeploySCW.sol
@@ -0,0 +1,23 @@
+
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import {MockWallet} from "../../../contracts/mocks/MockWallet.sol";
+import {MockWalletFactory} from "../../../contracts/mocks/MockWalletFactory.sol";
+
+contract DeploySCWallet is Test {
+    MockWallet public mockWalletModule;
+    MockWallet public scw;
+    MockWalletFactory public scmf;
+    address public scwAddress;
+
+    function run(bytes32 salt) external returns (address, address) {
+        scmf = new MockWalletFactory();
+        mockWalletModule = new MockWallet();
+        scmf.deploy(address(mockWalletModule), salt);
+        scwAddress = scmf.getAddress(address(mockWalletModule), salt);
+        scw = MockWallet(scwAddress);
+        return (scwAddress, address(mockWalletModule));
+    }
+}

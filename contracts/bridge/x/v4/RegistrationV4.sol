cc51ee3934b4ecb0a7c6b927e058d388c3e01344
Bryan Dichtl <bryan.dichtl@immutable.com>
2024-02-16 16:36:52 +1100

    Use error type instead of `require`

--- contracts/bridge/x/v4/RegistrationV4.sol
+++ contracts/bridge/x/v4/RegistrationV4.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.8.19;
 
 import {CoreV4} from "./CoreV4.sol";
 
+/**
+* Emitted when there are no funds to withdraw when calling `withdrawAll`.
+*/
+error NoFundsToWithdraw(uint256 ethKey, uint256 starkKey);
+
+/**
+ * RegistrationV4 is a wrapper around the StarkEx contract to provide a more user-friendly interface for executing multiple transactions on the StarkEx contract at once.
+ * This contract is not upgradeable. If an issue is found with this contract, a new version will be deployed.
+ */
 contract RegistrationV4 {
     CoreV4 public immutable imx;
 
@@ -31,7 +40,9 @@ contract RegistrationV4 {
     function withdrawAll(uint256 ethKey, uint256 starkKey, uint256 assetType) public {
         uint256 ethKeyBalance = imx.getWithdrawalBalance(ethKey, assetType);
         uint256 starkKeyBalance = imx.getWithdrawalBalance(starkKey, assetType);
-        require(ethKeyBalance != 0 || starkKeyBalance != 0, "No funds to withdraw");
+        if (ethKeyBalance == 0 && starkKeyBalance == 0) {
+            revert NoFundsToWithdraw(ethKey, starkKey);
+        }
 
         if (ethKeyBalance > 0) {
             imx.withdraw(ethKey, assetType);

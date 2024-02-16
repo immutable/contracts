8b193fabaf8e9ff6e3af85fd3c9eeb6ccacdcf13
Bryan Dichtl <bryan.dichtl@immutable.com>
2024-02-16 16:37:15 +1100

    Remove solc warnings from mock contract

--- test/bridge/x/v4/MockCoreV4.sol
+++ test/bridge/x/v4/MockCoreV4.sol
@@ -26,11 +26,11 @@ contract MockCoreV4 is CoreV4 {
         revert();
     }
 
-    function VERSION() external view override returns (string memory) {
+    function VERSION() external pure override returns (string memory) {
         return "4.0.1";
     }
 
-    function initialize(bytes memory data) external override {
+    function initialize(bytes memory) external pure override {
         revert("Not implemented");
     }
 
@@ -38,147 +38,139 @@ contract MockCoreV4 is CoreV4 {
         revert();
     }
 
-    function DEPOSIT_CANCEL_DELAY() external view override returns (uint256) {
+    function DEPOSIT_CANCEL_DELAY() external pure override returns (uint256) {
         return 0;
     }
 
-    function FREEZE_GRACE_PERIOD() external view override returns (uint256) {
+    function FREEZE_GRACE_PERIOD() external pure override returns (uint256) {
         return 0;
     }
 
-    function MAIN_GOVERNANCE_INFO_TAG() external view override returns (string memory) {
+    function MAIN_GOVERNANCE_INFO_TAG() external pure override returns (string memory) {
         return "governance_info";
     }
 
-    function MAX_FORCED_ACTIONS_REQS_PER_BLOCK() external view override returns (uint256) {
+    function MAX_FORCED_ACTIONS_REQS_PER_BLOCK() external pure override returns (uint256) {
         return 0;
     }
 
-    function MAX_VERIFIER_COUNT() external view override returns (uint256) {
+    function MAX_VERIFIER_COUNT() external pure override returns (uint256) {
         return 0;
     }
 
-    function UNFREEZE_DELAY() external view override returns (uint256) {
+    function UNFREEZE_DELAY() external pure override returns (uint256) {
         return 0;
     }
 
-    function VERIFIER_REMOVAL_DELAY() external view override returns (uint256) {
+    function VERIFIER_REMOVAL_DELAY() external pure override returns (uint256) {
         return 0;
     }
 
-    function announceAvailabilityVerifierRemovalIntent(address verifier) external override {
+    function announceAvailabilityVerifierRemovalIntent(address) external pure override {
         revert("Not implemented");
     }
 
-    function announceVerifierRemovalIntent(address verifier) external override {
+    function announceVerifierRemovalIntent(address) external pure override {
         revert("Not implemented");
     }
 
-    function getRegisteredAvailabilityVerifiers() external view override returns (address[] memory _verifers) {
+    function getRegisteredAvailabilityVerifiers() external pure override returns (address[] memory _verifers) {
         // Placeholder implementation, returning an empty array
         address[] memory verifiers;
         return verifiers;
     }
 
-    function getRegisteredVerifiers() external view override returns (address[] memory _verifers) {
+    function getRegisteredVerifiers() external pure override returns (address[] memory _verifers) {
         // Placeholder implementation, returning an empty array
         address[] memory verifiers;
         return verifiers;
     }
 
-    function isAvailabilityVerifier(address verifierAddress) external view override returns (bool) {
+    function isAvailabilityVerifier(address) external pure override returns (bool) {
         return false;
     }
 
-    function isFrozen() external view override returns (bool) {
+    function isFrozen() external pure override returns (bool) {
         return false;
     }
 
-    function isVerifier(address verifierAddress) external view override returns (bool) {
+    function isVerifier(address) external pure override returns (bool) {
         return false;
     }
 
-    function mainAcceptGovernance() external override {
+    function mainAcceptGovernance() external pure override {
         revert("Not implemented");
     }
 
-    function mainCancelNomination() external override {
+    function mainCancelNomination() external pure override {
         revert("Not implemented");
     }
 
-    function mainIsGovernor(address testGovernor) external view override returns (bool) {
+    function mainIsGovernor(address) external pure override returns (bool) {
         return false;
     }
 
-    function mainNominateNewGovernor(address newGovernor) external override {
+    function mainNominateNewGovernor(address) external pure override {
         revert("Not implemented");
     }
 
-    function mainRemoveGovernor(address governorForRemoval) external override {
+    function mainRemoveGovernor(address) external pure override {
         revert("Not implemented");
     }
 
-    function registerAvailabilityVerifier(address verifier, string memory identifier) external override {
+    function registerAvailabilityVerifier(address, string memory) external pure override {
         revert("Not implemented");
     }
 
-    function registerVerifier(address verifier, string memory identifier) external override {
+    function registerVerifier(address, string memory) external pure override {
         revert("Not implemented");
     }
 
-    function removeAvailabilityVerifier(address verifier) external override {
+    function removeAvailabilityVerifier(address) external pure override {
         revert("Not implemented");
     }
 
-    function removeVerifier(address verifier) external override {
+    function removeVerifier(address) external pure override {
         revert("Not implemented");
     }
 
-    function unFreeze() external override {
+    function unFreeze() external pure override {
         revert("Not implemented");
     }
 
-    function defaultVaultWithdrawalLock() external view override returns (uint256) {
+    function defaultVaultWithdrawalLock() external pure override returns (uint256) {
         return 0;
     }
 
-    function deposit(uint256 starkKey, uint256 assetType, uint256 vaultId) external payable override {
+    function deposit(uint256, uint256, uint256) external payable override {
         revert("Not implemented");
     }
 
-    function deposit(uint256 starkKey, uint256 assetType, uint256 vaultId, uint256 quantizedAmount) external override {
+    function deposit(uint256, uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function depositCancel(uint256 starkKey, uint256 assetId, uint256 vaultId) external override {
+    function depositCancel(uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function depositERC20(uint256 starkKey, uint256 assetType, uint256 vaultId, uint256 quantizedAmount)
-        external
-        pure
-        override
-    {
+    function depositERC20(uint256, uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function depositEth(uint256 starkKey, uint256 assetType, uint256 vaultId) external payable override {
+    function depositEth(uint256, uint256, uint256) external payable override {
         revert("Not implemented");
     }
 
-    function depositNft(uint256 starkKey, uint256 assetType, uint256 vaultId, uint256 tokenId) external pure override {
+    function depositNft(uint256, uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function depositNftReclaim(uint256 starkKey, uint256 assetType, uint256 vaultId, uint256 tokenId)
-        external
-        pure
-        override
-    {
+    function depositNftReclaim(uint256, uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function depositReclaim(uint256 starkKey, uint256 assetId, uint256 vaultId) external pure override {
+    function depositReclaim(uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
@@ -186,29 +178,19 @@ contract MockCoreV4 is CoreV4 {
         return 0;
     }
 
-    function getActionHashByIndex(uint256 actionIndex) external pure override returns (bytes32) {
+    function getActionHashByIndex(uint256) external pure override returns (bytes32) {
         revert("Not implemented");
     }
 
-    function getAssetInfo(uint256 assetType) external pure override returns (bytes memory) {
+    function getAssetInfo(uint256) external pure override returns (bytes memory) {
         revert("Not implemented");
     }
 
-    function getCancellationRequest(uint256 starkKey, uint256 assetId, uint256 vaultId)
-        external
-        pure
-        override
-        returns (uint256)
-    {
+    function getCancellationRequest(uint256, uint256, uint256) external pure override returns (uint256) {
         revert("Not implemented");
     }
 
-    function getDepositBalance(uint256 starkKey, uint256 assetId, uint256 vaultId)
-        external
-        pure
-        override
-        returns (uint256)
-    {
+    function getDepositBalance(uint256, uint256, uint256) external pure override returns (uint256) {
         revert("Not implemented");
     }
 
@@ -222,20 +204,15 @@ contract MockCoreV4 is CoreV4 {
         return ownerKey == (ownerKey & MASK_ADDRESS) ? address(uint160(ownerKey)) : address(0x0);
     }
 
-    function getFullWithdrawalRequest(uint256 starkKey, uint256 vaultId) external pure override returns (uint256) {
+    function getFullWithdrawalRequest(uint256, uint256) external pure override returns (uint256) {
         revert("Not implemented");
     }
 
-    function getQuantizedDepositBalance(uint256 starkKey, uint256 assetId, uint256 vaultId)
-        external
-        pure
-        override
-        returns (uint256)
-    {
+    function getQuantizedDepositBalance(uint256, uint256, uint256) external pure override returns (uint256) {
         revert("Not implemented");
     }
 
-    function getQuantum(uint256 presumedAssetType) external pure override returns (uint256) {
+    function getQuantum(uint256) external pure override returns (uint256) {
         revert("Not implemented");
     }
 
@@ -247,11 +224,11 @@ contract MockCoreV4 is CoreV4 {
         return pendingWithdrawals[ownerKey][assetId];
     }
 
-    function isAssetRegistered(uint256 assetType) external pure override returns (bool) {
+    function isAssetRegistered(uint256) external pure override returns (bool) {
         revert("Not implemented");
     }
 
-    function isTokenAdmin(address testedAdmin) external pure override returns (bool) {
+    function isTokenAdmin(address) external pure override returns (bool) {
         revert("Not implemented");
     }
 
@@ -263,24 +240,15 @@ contract MockCoreV4 is CoreV4 {
         revert("Not implemented");
     }
 
-    function registerAndDepositERC20(
-        address ethKey,
-        uint256 starkKey,
-        bytes memory signature,
-        uint256 assetType,
-        uint256 vaultId,
-        uint256 quantizedAmount
-    ) external pure override {
+    function registerAndDepositERC20(address, uint256, bytes memory, uint256, uint256, uint256)
+        external
+        pure
+        override
+    {
         revert("Not implemented");
     }
 
-    function registerAndDepositEth(
-        address ethKey,
-        uint256 starkKey,
-        bytes memory signature,
-        uint256 assetType,
-        uint256 vaultId
-    ) external payable override {
+    function registerAndDepositEth(address, uint256, bytes memory, uint256, uint256) external payable override {
         revert("Not implemented");
     }
 
@@ -298,11 +266,11 @@ contract MockCoreV4 is CoreV4 {
         emit LogUserRegistered(ethKey, starkKey, msg.sender);
     }
 
-    function registerSender(uint256 starkKey, bytes memory starkSignature) external pure override {
+    function registerSender(uint256, bytes memory) external pure override {
         revert("Not implemented");
     }
 
-    function registerToken(uint256 assetType, bytes memory assetInfo) external pure override {
+    function registerToken(uint256, bytes memory) external pure override {
         revert("Not implemented");
     }
 
@@ -310,15 +278,15 @@ contract MockCoreV4 is CoreV4 {
         tokenContracts[assetType] = tokenContract;
     }
 
-    function registerToken(uint256 assetType, bytes memory assetInfo, uint256 quantum) external pure override {
+    function registerToken(uint256, bytes memory, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function registerTokenAdmin(address newAdmin) external pure override {
+    function registerTokenAdmin(address) external pure override {
         revert("Not implemented");
     }
 
-    function unregisterTokenAdmin(address oldAdmin) external pure override {
+    function unregisterTokenAdmin(address) external pure override {
         revert("Not implemented");
     }
 
@@ -332,6 +300,7 @@ contract MockCoreV4 is CoreV4 {
 
         // Transfer funds.
         (bool success,) = recipient.call{value: amount}(""); // NOLINT: low-level-calls.
+        require(success, "TRANSFER_ERROR");
         emit LogWithdrawalPerformed(ownerKey, assetType, amount, amount, recipient);
     }
 
@@ -339,7 +308,7 @@ contract MockCoreV4 is CoreV4 {
         address payable recipient = payable(getEthKey(ownerKey));
         require(recipient != ZERO_ADDRESS, "USER_UNREGISTERED");
 
-        (uint256 tokenId, bytes memory blueprint) = Minting.split(mintingBlob);
+        (uint256 tokenId,) = Minting.split(mintingBlob);
         uint256 assetId = assetType + tokenId;
         uint256 amount = pendingWithdrawals[ownerKey][assetId];
         pendingWithdrawals[ownerKey][assetId] = 0;
@@ -383,11 +352,7 @@ contract MockCoreV4 is CoreV4 {
         return 0;
     }
 
-    function escape(uint256 starkKey, uint256 vaultId, uint256 assetId, uint256 quantizedAmount)
-        external
-        pure
-        override
-    {
+    function escape(uint256, uint256, uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
@@ -415,51 +380,47 @@ contract MockCoreV4 is CoreV4 {
         return 0;
     }
 
-    function isOperator(address testedOperator) external pure override returns (bool) {
+    function isOperator(address) external pure override returns (bool) {
         return false;
     }
 
-    function registerOperator(address newOperator) external pure override {
+    function registerOperator(address) external pure override {
         revert("Not implemented");
     }
 
-    function unregisterOperator(address removedOperator) external pure override {
+    function unregisterOperator(address) external pure override {
         revert("Not implemented");
     }
 
-    function updateState(uint256[] memory publicInput, uint256[] memory applicationData) external pure override {
+    function updateState(uint256[] memory, uint256[] memory) external pure override {
         revert("Not implemented");
     }
 
-    function freezeRequest(uint256 starkKey, uint256 vaultId) external pure override {
+    function freezeRequest(uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function fullWithdrawalRequest(uint256 starkKey, uint256 vaultId) external pure override {
+    function fullWithdrawalRequest(uint256, uint256) external pure override {
         revert("Not implemented");
     }
 
-    function depositERC20ToVault(uint256 assetId, uint256 vaultId, uint256 quantizedAmount) external pure {
+    function depositERC20ToVault(uint256, uint256, uint256) external pure {
         revert("Not implemented");
     }
 
-    function depositEthToVault(uint256 assetId, uint256 vaultId) external payable {
+    function depositEthToVault(uint256, uint256) external payable {
         revert("Not implemented");
     }
 
-    function getQuantizedVaultBalance(address ethKey, uint256 assetId, uint256 vaultId)
-        external
-        pure
-        returns (uint256)
-    {
+    function getQuantizedVaultBalance(address, uint256, uint256) external pure returns (uint256) {
         revert("Not implemented");
     }
 
-    function getVaultBalance(address ethKey, uint256 assetId, uint256 vaultId) external pure returns (uint256) {
+    function getVaultBalance(address, uint256, uint256) external pure returns (uint256) {
         revert("Not implemented");
     }
 
-    function getVaultWithdrawalLock(address ethKey, uint256 assetId, uint256 vaultId) external pure returns (uint256) {
+    function getVaultWithdrawalLock(address, uint256, uint256) external pure returns (uint256) {
         revert("Not implemented");
     }
 
@@ -467,26 +428,23 @@ contract MockCoreV4 is CoreV4 {
         revert("Not implemented");
     }
 
-    function isVaultLocked(address ethKey, uint256 assetId, uint256 vaultId) external pure returns (bool) {
+    function isVaultLocked(address, uint256, uint256) external pure returns (bool) {
         revert("Not implemented");
     }
 
-    function lockVault(uint256 assetId, uint256 vaultId, uint256 lockTime) external pure {
+    function lockVault(uint256, uint256, uint256) external pure {
         revert("Not implemented");
     }
 
-    function setDefaultVaultWithdrawalLock(uint256 newDefaultTime) external pure {
+    function setDefaultVaultWithdrawalLock(uint256) external pure {
         revert("Not implemented");
     }
 
-    function updateImplementationActivationTime(address implementation, bytes memory data, bool finalize)
-        external
-        pure
-    {
+    function updateImplementationActivationTime(address, bytes memory, bool) external pure {
         revert("Not implemented");
     }
 
-    function withdrawFromVault(uint256 assetId, uint256 vaultId, uint256 quantizedAmount) external pure {
+    function withdrawFromVault(uint256, uint256, uint256) external pure {
         revert("Not implemented");
     }
 }

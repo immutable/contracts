e3d610947f2519c40d8f5badb60781d3a6ca7360
Bryan Dichtl <bryan.dichtl@immutable.com>
2024-02-21 13:36:43 +1100

    Add deployment SANDBOX and MAINNET deployment scripts
    Resolves: [CORE-2047]

---
+++ script/bridge/x/v4/DeployRegistrationV4Dev.s.sol
@@ -0,0 +1,22 @@
+// Copyright (c) Immutable Pty Ltd 2018 - 2024
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {RegistrationV4} from "../../../../contracts/bridge/x/v4/RegistrationV4.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployRegistrationV4Dev is Script {
+    address public DEV_CONTRACT = 0x590C809bd5FF50DCb39e4320b60139B29B880174;
+    uint256 private SEPOLIA_CHAIN_ID = 11155111;
+
+    RegistrationV4 public registration;
+
+    function run() external returns (RegistrationV4) {
+        require(block.chainid == SEPOLIA_CHAIN_ID, "wrong chain id, please use sepolia chain");
+
+        vm.startBroadcast();
+        registration = new RegistrationV4(payable(DEV_CONTRACT));
+        vm.stopBroadcast();
+        return registration;
+    }
+}
--- script/bridge/x/v4/DeployRegistrationV4Sandbox.s.sol
+++ script/bridge/x/v4/DeployRegistrationV4Sandbox.s.sol
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {RegistrationV4} from "../contracts/bridge/x/v4/RegistrationV4.sol";
+import {RegistrationV4} from "../../../../contracts/bridge/x/v4/RegistrationV4.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract DeployRegistrationV4Dev is Script {
-    address public DEV_CONTRACT = 0x590C809bd5FF50DCb39e4320b60139B29B880174;
+contract DeployRegistrationV4Sandbox is Script {
+    address public SANDBOX_CONTRACT = 0x2d5C349fD8464DA06a3f90b4B0E9195F3d1b7F98;
     uint256 private SEPOLIA_CHAIN_ID = 11155111;
 
     RegistrationV4 public registration;
@@ -15,7 +15,7 @@ contract DeployRegistrationV4Dev is Script {
         require(block.chainid == SEPOLIA_CHAIN_ID, "wrong chain id, please use sepolia chain");
 
         vm.startBroadcast();
-        registration = new RegistrationV4(payable(DEV_CONTRACT));
+        registration = new RegistrationV4(payable(SANDBOX_CONTRACT));
         vm.stopBroadcast();
         return registration;
     }

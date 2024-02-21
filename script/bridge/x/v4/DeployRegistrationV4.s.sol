a35d4d9044aa6beff88441e5ce735a24142677fa
Bryan Dichtl <bryan.dichtl@immutable.com>
2024-02-21 13:43:18 +1100

    Fix mainnet script

--- script/bridge/x/v4/DeployRegistrationV4.s.sol
+++ script/bridge/x/v4/DeployRegistrationV4.s.sol
@@ -5,17 +5,17 @@ pragma solidity ^0.8.20;
 import {RegistrationV4} from "../../../../contracts/bridge/x/v4/RegistrationV4.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract DeployRegistrationV4Dev is Script {
-    address public PROD_CONTRACT = 0x5FDCCA53617f4d2b9134B29090C87D01058e27e9;
-    uint256 private SEPOLIA_CHAIN_ID = 1;
+contract DeployRegistrationV4 is Script {
+    address public MAINNET_CONTRACT = 0x5FDCCA53617f4d2b9134B29090C87D01058e27e9;
+    uint256 private MAINNET_CHAIN_ID = 1;
 
     RegistrationV4 public registration;
 
     function run() external returns (RegistrationV4) {
-        require(block.chainid == SEPOLIA_CHAIN_ID, "wrong chain id, please use mainnet chain");
+        require(block.chainid == MAINNET_CHAIN_ID, "wrong chain id, please use mainnet chain");
 
         vm.startBroadcast();
-        registration = new RegistrationV4(payable(PROD_CONTRACT));
+        registration = new RegistrationV4(payable(MAINNET_CONTRACT));
         vm.stopBroadcast();
         return registration;
     }

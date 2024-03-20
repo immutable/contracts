c64efa1b15f0af81608e2190ec92c613f6e73658
jason li <jason.li@immutable.com>
2024-03-20 12:23:02 +1100

    [gms-1526] add ts docs to immutableerc1155

--- contracts/token/erc1155/preset/draft-ImmutableERC1155.sol
+++ contracts/token/erc1155/preset/draft-ImmutableERC1155.sol
@@ -21,6 +21,13 @@ contract ImmutableERC1155 is ImmutableERC1155Base {
      * Sets the default admin to `owner`
      * Sets the `baseURI`
      * Sets the royalty receiver and amount (this can not be changed once set)
+     * @param owner The address that will be granted the `DEFAULT_ADMIN_ROLE`
+     * @param name_ The name of the collection
+     * @param baseURI_ The base URI for the collection
+     * @param contractURI_ The contract URI for the collection
+     * @param _operatorAllowlist The address of the OAL
+     * @param _receiver The address that will receive the royalty payments
+     * @param _feeNumerator The percentage of the sale price that will be paid as a royalty
      */
     constructor(
         address owner,
@@ -34,10 +41,24 @@ contract ImmutableERC1155 is ImmutableERC1155Base {
 
     ///     =====   External functions  =====
 
+    /**
+     * @notice Mints a new token
+     * @param to The address that will receive the minted tokens
+     * @param id The id of the token to mint
+     * @param value The amount of tokens to mint
+     * @param data Additional data
+     */
     function safeMint(address to, uint256 id, uint256 value, bytes memory data) external onlyRole(MINTER_ROLE) {
         super._mint(to, id, value, data);
     }
 
+    /**
+     * @notice Mints a batch of new tokens with different ids to the same recipient
+     * @param to The address that will receive the minted tokens
+     * @param ids The ids of the tokens to mint
+     * @param values The amounts of tokens to mint
+     * @param data Additional data
+     */
     function safeMintBatch(
         address to,
         uint256[] calldata ids,

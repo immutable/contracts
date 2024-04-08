fb65acd91a2d57edab23d5a5adaf007f4d65147c
Pano Skylakis <pano.skylakis@immutable.com>
2024-04-08 17:58:36 +1200

    Feat: add Gem Game contract

---
+++ contracts/games/gems/GemGame.sol
@@ -0,0 +1,14 @@
+// Copyright (c) Immutable Pty Ltd 2018 - 2024
+// SPDX-License-Identifier: Apache 2
+pragma solidity ^0.8.20;
+
+/**
+ * @title GemGame - A simple contract that emits an event.
+ */
+contract GemGame {
+    event GemEarned(address indexed account);
+
+    function earnGem() external {
+        emit GemEarned(msg.sender);
+    }
+}

4df56f65ae946fc92f9c80ebf521867237e96f8a
Pano Skylakis <pano.skylakis@immutable.com>
2024-04-09 11:50:05 +1200

    Housekeeping

--- contracts/games/gems/GemGame.sol
+++ contracts/games/gems/GemGame.sol
@@ -1,13 +1,18 @@
 // Copyright (c) Immutable Pty Ltd 2018 - 2024
 // SPDX-License-Identifier: Apache 2
-pragma solidity =0.8.20;
+pragma solidity ^0.8.20;
 
 /**
- * @title GemGame - A simple contract that emits an event.
+ * @title GemGame - A simple contract that emits an event for the purpose of indexing off-chain
+ * @dev The GemGame contract is not designed to be upgradeable or extended
  */
 contract GemGame {
+    /// @notice Indicates that an account has earned a gem
     event GemEarned(address indexed account, uint256 timestamp);
 
+    /**
+     * @notice Function that emits a `GemEarned` event
+     */
     function earnGem() external {
         emit GemEarned(msg.sender, block.timestamp);
     }
--- contracts/games/gems/README.md
+++ contracts/games/gems/README.md
@@ -1,1 +1,3 @@
 # Gem (GM) Game
+
+The GemGame contract emits a single event for the purpose of indexing off-chain.

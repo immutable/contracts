32d9d8820a57002e1cbbb3912bf9e213bb5f66dd
Allan Almeida <allan.almeida@immutable.com>
2024-04-09 23:04:06 +1000

    export typescript abis

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { immutableErc721Abi as ImmutableERC721, immutableErc721MintByIdAbi as ImmutableERC721MintByID } from "./abi";
+
+export { ImmutableERC721, ImmutableERC721MintByID };
---
+++ wagmi.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "@wagmi/cli";
+import { Abi } from "abitype";
+
+import ImmutableERC721 from "./foundry-out/ImmutableERC721.sol/ImmutableERC721.json";
+import ImmutableERC721MintByID from "./foundry-out/ImmutableERC721MintByID.sol/ImmutableERC721MintByID.json";
+
+// https://github.com/wevm/viem/discussions/1009
+export default defineConfig({
+  out: "abi/index.ts",
+  contracts: [
+    {
+      name: "ImmutableERC721",
+      abi: ImmutableERC721.abi as Abi,
+    },
+    {
+      name: "ImmutableERC721MintByID",
+      abi: ImmutableERC721MintByID.abi as Abi,
+    },
+  ],
+});

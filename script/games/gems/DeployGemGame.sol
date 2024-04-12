32dc5d1347c4fc04905bf48c08b6938b10c4ae7e
Pano Skylakis <pano.skylakis@immutable.com>
2024-04-12 17:08:15 +1200

    Refactor: add access control, add ability to pause

--- script/games/gems/DeployGemGame.sol
+++ script/games/gems/DeployGemGame.sol
@@ -26,33 +26,87 @@ interface IDeployer {
 
 struct DeploymentArgs {
     address signer;
-    address deployer;
+    address factory;
     string salt;
 }
 
+struct GemGameContractArgs {
+    address defaultAdmin;
+    address pauser;
+    address unpauser;
+}
+
 contract DeployGemGame is Test {
-    function deploy() external {
-        address signer = vm.envAddress("DEPLOYER_ADDRESS");
-        address deployer = vm.envAddress("OWNABLE_CREATE3_FACTORY_ADDRESS");
-        string memory salt = vm.envString("GEM_GAME_SALT");
+    function testDeploy() external {
+        /// @dev Fork the Immutable zkEVM testnet for this test
+        string memory rpcURL = "https://rpc.testnet.immutable.com";
+        vm.createSelectFork(rpcURL);
 
-        DeploymentArgs memory deploymentArgs = DeploymentArgs({signer: signer, deployer: deployer, salt: salt});
+        /// @dev These are Immutable zkEVM testnet values where necessary
+        DeploymentArgs memory deploymentArgs = DeploymentArgs({
+            signer: 0xdDA0d9448Ebe3eA43aFecE5Fa6401F5795c19333,
+            factory: 0x37a59A845Bb6eD2034098af8738fbFFB9D589610,
+            salt: "salty"
+        });
 
-        _deploy(deploymentArgs);
+        GemGameContractArgs memory gemGameContractArgs = GemGameContractArgs({
+            pauser: makeAddr("pause"),
+            unpauser: makeAddr("unpause"),
+            defaultAdmin: makeAddr("admin")
+        });
+
+        // Run deployment against forked testnet
+        GemGame deployedGemGameContract = _deploy(deploymentArgs, gemGameContractArgs);
+
+        assertEq(true, deployedGemGameContract.hasRole(keccak256("PAUSE"), gemGameContractArgs.pauser));
+        assertEq(true, deployedGemGameContract.hasRole(keccak256("UNPAUSE"), gemGameContractArgs.unpauser));
+        assertEq(
+            true,
+            deployedGemGameContract.hasRole(
+                deployedGemGameContract.DEFAULT_ADMIN_ROLE(), gemGameContractArgs.defaultAdmin
+            )
+        );
+
+        // The DEFAULT_ADMIN_ROLE should be revoked from the deployer account
+        assertEq(
+            false, deployedGemGameContract.hasRole(deployedGemGameContract.DEFAULT_ADMIN_ROLE(), deploymentArgs.signer)
+        );
     }
 
-    function _deploy(DeploymentArgs memory args) internal returns (GemGame gemGameContract) {
-        IDeployer ownableCreate3 = IDeployer(args.deployer);
+    function deploy() external {
+        address signer = vm.envAddress("DEPLOYER_ADDRESS");
+        address factory = vm.envAddress("OWNABLE_CREATE3_FACTORY_ADDRESS");
+        address defaultAdmin = vm.envAddress("DEFAULT_ADMIN");
+        address pauser = vm.envAddress("PAUSER");
+        address unpauser = vm.envAddress("UNPAUSER");
+        string memory salt = vm.envString("GEM_GAME_SALT");
+
+        DeploymentArgs memory deploymentArgs = DeploymentArgs({signer: signer, factory: factory, salt: salt});
+
+        GemGameContractArgs memory gemGameContractArgs =
+            GemGameContractArgs({defaultAdmin: defaultAdmin, pauser: pauser, unpauser: unpauser});
+
+        _deploy(deploymentArgs, gemGameContractArgs);
+    }
+
+    function _deploy(DeploymentArgs memory deploymentArgs, GemGameContractArgs memory gemGameContractArgs)
+        internal
+        returns (GemGame gemGameContract)
+    {
+        IDeployer ownableCreate3 = IDeployer(deploymentArgs.factory);
 
         // Create deployment bytecode and encode constructor args
-        bytes memory bytecode = abi.encodePacked(type(GemGame).creationCode);
+        bytes memory deploymentBytecode = abi.encodePacked(
+            type(GemGame).creationCode,
+            abi.encode(gemGameContractArgs.defaultAdmin, gemGameContractArgs.pauser, gemGameContractArgs.unpauser)
+        );
 
-        bytes32 saltBytes = keccak256(abi.encode(args.salt));
+        bytes32 saltBytes = keccak256(abi.encode(deploymentArgs.salt));
 
         /// @dev Deploy the contract via the Ownable CREATE3 factory
-        vm.startBroadcast(args.signer);
+        vm.startBroadcast(deploymentArgs.signer);
 
-        address gemGameContractAddress = ownableCreate3.deploy(bytecode, saltBytes);
+        address gemGameContractAddress = ownableCreate3.deploy(deploymentBytecode, saltBytes);
         gemGameContract = GemGame(gemGameContractAddress);
 
         vm.stopBroadcast();

9ca54f5261eb45adbf8826adbf6a73850551ceca
Pano Skylakis <pano.skylakis@immutable.com>
2024-04-12 16:04:32 +1200

    Feat: add time-based constraint to claims

--- contracts/games/gems/GemGame.sol
+++ contracts/games/gems/GemGame.sol
@@ -1,6 +1,9 @@
 // Copyright (c) Immutable Pty Ltd 2018 - 2024
 // SPDX-License-Identifier: Apache 2
+// solhint-disable not-rely-on-time
 pragma solidity ^0.8.19;
+
+error TooSoon();
 
 /**
  * @title GemGame - A simple contract that emits an event for the purpose of indexing off-chain
@@ -11,11 +14,24 @@ contract GemGame {
     /// @notice Indicates that an account has earned a gem
     event GemEarned(address indexed account, uint256 timestamp);
 
+    /// @notice Mapping of the last time an account earned a gem
+    mapping(address account => uint256 lastEarned) public accountLastEarned;
+
     /**
      * @notice Function that emits a `GemEarned` event
      */
     function earnGem() external {
-        // solhint-disable-next-line not-rely-on-time
+        // Get the timestamp of midnight UTC for the current block
+        uint256 utcMidnight = block.timestamp - block.timestamp % (24 * 60 * 60);
+
+        // Check if the account has already earned a gem today
+        // If their last earned timestamp is less than midnight UTC, they can earn a gem
+        if (accountLastEarned[msg.sender] < utcMidnight) revert TooSoon();
+
+        // Set the last earned timestamp to the current block timestamp
+        accountLastEarned[msg.sender] = block.timestamp;
+
+        // User has earned a gem
         emit GemEarned(msg.sender, block.timestamp);
     }
 }

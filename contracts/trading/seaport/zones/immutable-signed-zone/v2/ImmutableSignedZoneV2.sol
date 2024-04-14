ccba46b87b619a5c959f95b91fc239cf27424946
Leslie Fung <leslie.fung.lf@gmail.com>
2024-04-15 09:04:36 +1000

    Set solidity version to ^0.8.20

--- contracts/trading/seaport/zones/immutable-signed-zone/v2/ImmutableSignedZoneV2.sol
+++ contracts/trading/seaport/zones/immutable-signed-zone/v2/ImmutableSignedZoneV2.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2
 
 // solhint-disable-next-line compiler-version
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import {ZoneInterface} from "seaport/contracts/interfaces/ZoneInterface.sol";
 import {ZoneParameters, Schema, ReceivedItem} from "seaport-types/src/lib/ConsiderationStructs.sol";

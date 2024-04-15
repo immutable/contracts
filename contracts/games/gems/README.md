cb993e994674027b17b2547fd793c0acdd4c37e2
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2024-04-15 16:51:42 +1200

    Add AccessControl, Pausable to GemGame contract (#200)
    * Feat: add time-based constraint to claims
    
    * Refactor: add access control, add ability to pause
    
    * Feat: add earnGem call after deployment test
    
    * Fix: remove account mapping
    
    ---------
    
    Co-authored-by: Pano Skylakis <pano.skylakis@immutable.com>

--- contracts/games/gems/README.md
+++ contracts/games/gems/README.md
@@ -16,3 +16,16 @@ Contract threat models and audits:
 | Description               | Date             |Version Audited  | Link to Report |
 |---------------------------|------------------|-----------------|----------------|
 | Not audited and no threat model              | -                | -               | -              |
+
+
+**Deploy and verify using CREATE3 factory contract:**
+
+This repo includes a script for deploying via a CREATE3 factory contract. The script is defined as a test contract as per the examples [here](https://book.getfoundry.sh/reference/forge/forge-script#examples) and can be found in `./script/games/gems/DeployGemGame.sol`.
+
+See the `.env.example` for required environment variables.
+
+```sh
+forge script script/games/gems/DeployGemGame.sol --tc DeployGemGame --sig "deploy()" -vvv --rpc-url {rpc-url} --broadcast --verifier-url https://explorer.immutable.com/api --verifier blockscout --verify --gas-price 10gwei
+```
+
+Optionally, you can also specify `--ledger` or `--trezor` for hardware deployments. See docs [here](https://book.getfoundry.sh/reference/forge/forge-script#wallet-options---hardware-wallet).
--- test/games/gems/GemGame.t.sol
+++ test/games/gems/GemGame.t.sol
@@ -1,22 +1,68 @@
 // Copyright Immutable Pty Ltd 2018 - 2024
 // SPDX-License-Identifier: Apache 2.0
-pragma solidity ^0.8.20;
+// solhint-disable not-rely-on-time
+
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
-import {GemGame} from "../../../contracts/games/gems/GemGame.sol";
+import {GemGame, Unauthorized, ContractPaused} from "../../../contracts/games/gems/GemGame.sol";
 
 contract GemGameTest is Test {
     event GemEarned(address indexed account, uint256 timestamp);
 
-    GemGame gemGame;
+    GemGame private _gemGame;
 
     function setUp() public {
-        gemGame = new GemGame();
+        _gemGame = new GemGame(address(this), address(this), address(this));
     }
 
-    function testEarnGem_EmitsGemEarnedEvent() public {
+    function testEarnGemEmitsGemEarnedEvent() public {
         vm.expectEmit(true, true, false, false);
         emit GemEarned(address(this), block.timestamp);
-        gemGame.earnGem();
+        _gemGame.earnGem();
+    }
+
+    function testEarnGemContractPausedReverts() public {
+        // pause the contract
+        _gemGame.pause();
+
+        // attempt to earn a gem
+        vm.expectRevert(ContractPaused.selector);
+        _gemGame.earnGem();
+    }
+
+    function testPausePausesContract() public {
+        // pause the contract
+        _gemGame.pause();
+
+        assertEq(_gemGame.paused(), true, "GemGame should be paused");
+    }
+
+    function testPauseWithoutPauseRoleReverts() public {
+        // revoke the pause role
+        _gemGame.revokeRole(keccak256("PAUSE"), address(this));
+
+        // attempt to pause
+        vm.expectRevert(Unauthorized.selector);
+        _gemGame.pause();
+    }
+
+    function testUnpauseUnpausesContract() public {
+        // pause the contract
+        _gemGame.pause();
+
+        // unpause the contract
+        _gemGame.unpause();
+
+        assertEq(_gemGame.paused(), false, "GemGame should be unpaused");
+    }
+
+    function testUnpauseWithoutPauseRoleReverts() public {
+        // revoke the unpause role
+        _gemGame.revokeRole(keccak256("UNPAUSE"), address(this));
+
+        // attempt to unpause
+        vm.expectRevert(Unauthorized.selector);
+        _gemGame.unpause();
     }
 }

fe21774b59dbd5c8810bb8d2dacabc7323f3b204
Allan Almeida <allan.almeida@immutable.com>
2024-04-26 16:59:04 +1000

    add ImmutableERC1155

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -3,6 +3,7 @@ import { Abi } from "abitype";
 
 import ImmutableERC721 from "./foundry-out/ImmutableERC721.sol/ImmutableERC721.json";
 import ImmutableERC721MintByID from "./foundry-out/ImmutableERC721MintByID.sol/ImmutableERC721MintByID.json";
+import ImmutableERC1155 from "./foundry-out/ImmutableERC1155.sol/ImmutableERC1155.json";
 
 // https://github.com/wevm/viem/discussions/1009
 export default defineConfig({
@@ -15,6 +16,10 @@ export default defineConfig({
     {
       name: "ImmutableERC721MintByID",
       abi: ImmutableERC721MintByID.abi as Abi,
+    },
+    {
+      name: "ImmutableERC1155",
+      abi: ImmutableERC1155.abi as Abi,
     },
   ],
 });

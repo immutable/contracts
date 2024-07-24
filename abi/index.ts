9ca1db3b6965facab9f300d4ee7105156bfeb453
Luã de Souza <lua.desouza@immutable.com>
2024-07-24 16:01:18 +1000

    Export ABI for GuardedMulticaller contract (#236)

--- abi/index.ts
+++ abi/index.ts
@@ -1,7 +1,8 @@
 import {
+  guardedMulticallerAbi as GuardedMulticallerAbi,
   immutableErc721Abi as ImmutableERC721Abi,
   immutableErc721MintByIdAbi as ImmutableERC721MintByIdAbi,
   immutableErc1155Abi as ImmutableERC1155Abi,
 } from "./generated";
 
-export { ImmutableERC721Abi, ImmutableERC721MintByIdAbi, ImmutableERC1155Abi };
+export { GuardedMulticallerAbi, ImmutableERC721Abi, ImmutableERC721MintByIdAbi, ImmutableERC1155Abi };
--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@wagmi/cli";
 import { Abi } from "abitype";
 
+import GuardedMulticaller from "./foundry-out/GuardedMulticaller.sol/GuardedMulticaller.json";
 import ImmutableERC721 from "./foundry-out/ImmutableERC721.sol/ImmutableERC721.json";
 import ImmutableERC721MintByID from "./foundry-out/ImmutableERC721MintByID.sol/ImmutableERC721MintByID.json";
 import ImmutableERC1155 from "./foundry-out/ImmutableERC1155.sol/ImmutableERC1155.json";
@@ -9,6 +10,10 @@ import ImmutableERC1155 from "./foundry-out/ImmutableERC1155.sol/ImmutableERC115
 export default defineConfig({
   out: "abi/generated.ts",
   contracts: [
+    {
+      name: "GuardedMulticaller",
+      abi: GuardedMulticaller.abi as Abi,
+    },
     {
       name: "ImmutableERC721",
       abi: ImmutableERC721.abi as Abi,

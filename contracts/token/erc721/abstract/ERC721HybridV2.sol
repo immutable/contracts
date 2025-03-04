270d766339cca38587f440f8bdaeb0f69fdd1c35
Peter Robinson <drinkcoffee@eml.cc>
2025-03-04 14:07:26 +1000

    Add virtual to totalSupply

--- contracts/token/erc721/abstract/ERC721HybridV2.sol
+++ contracts/token/erc721/abstract/ERC721HybridV2.sol
@@ -81,7 +81,7 @@ abstract contract ERC721HybridV2 is ERC721PsiBurnableV2, ERC721, IImmutableERC72
     /* @notice Overwritten functions with combined implementations, supply for the collection is summed as they
      *  are tracked differently by each minting strategy
      */
-    function totalSupply() public view override(ERC721PsiV2) returns (uint256) {
+    function totalSupply() public view virtual override(ERC721PsiV2) returns (uint256) {
         return ERC721PsiV2.totalSupply() + _idMintTotalSupply;
     }
 

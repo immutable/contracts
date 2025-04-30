2c88997e69a5538da055921b96aba9f2c58986a9
Peter Robinson <drinkcoffee@eml.cc>
2025-04-30 16:14:47 +1000

    Add more tests

---
+++ test/staking/StakeHolderTimeDelayWIMX.t.sol
@@ -0,0 +1,43 @@
+// Copyright Immutable Pty Ltd 2018 - 2024
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity >=0.8.19 <0.8.29;
+
+// solhint-disable-next-line no-global-import
+import "forge-std/Test.sol";
+import {StakeHolderWIMX} from "../../contracts/staking/StakeHolderWIMX.sol";
+import {IStakeHolder} from "../../contracts/staking/IStakeHolder.sol";
+import {StakeHolderTimeDelayBaseTest} from "./StakeHolderTimeDelayBase.t.sol";
+import {ERC1967Proxy} from "openzeppelin-contracts-4.9.3/proxy/ERC1967/ERC1967Proxy.sol";
+import {WIMX} from "../../contracts/staking/WIMX.sol";
+import {StakeHolderBase} from "../../contracts/staking/StakeHolderBase.sol";
+
+contract StakeHolderWIMXV2 is StakeHolderWIMX {
+    function upgradeStorage(bytes memory /* _data */) external override(StakeHolderBase) {
+        version = 1;
+    }
+}
+
+
+contract StakeHolderTimeDelayERC20Test is StakeHolderTimeDelayBaseTest {
+    WIMX erc20;
+
+    function setUp() public override {
+        super.setUp();
+
+        erc20 = new WIMX();
+
+        StakeHolderWIMX impl = new StakeHolderWIMX();
+
+        bytes memory initData = abi.encodeWithSelector(
+            StakeHolderWIMX.initialize.selector, address(stakeHolderTimeDelay), address(stakeHolderTimeDelay), 
+                distributeAdmin, address(erc20)
+        );
+
+        ERC1967Proxy proxy = new ERC1967Proxy(address(impl), initData);
+        stakeHolder = IStakeHolder(address(proxy));
+    }
+
+    function _deployV2() internal override returns(IStakeHolder) {
+        return IStakeHolder(address(new StakeHolderWIMXV2()));
+    }
+}

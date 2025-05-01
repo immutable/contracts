b7317acb2626838d826f5bc6782fa1d2483c47ce
Peter Robinson <drinkcoffee@eml.cc>
2025-05-01 13:47:34 +1000

    Change name from ETH to IMX

--- contracts/staking/IWIMX.sol
+++ contracts/staking/IWIMX.sol
@@ -6,30 +6,30 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /*
  * @notice Interface for the Wrapped IMX (wIMX) contract.
- * @dev Based on the interface for the [Wrapped ETH contract](https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2#code)
+ * @dev Based on the interface for the [Wrapped IMX contract](https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2#code)
  */
 interface IWIMX is IERC20 {
     /**
-     * @notice Emitted when native ETH is deposited to the contract, and a corresponding amount of wETH are minted
+     * @notice Emitted when native IMX is deposited to the contract, and a corresponding amount of wIMX are minted
      * @param account The address of the account that deposited the tokens.
      * @param value The amount of tokens that were deposited.
      */
     event Deposit(address indexed account, uint256 value);
 
     /**
-     * @notice Emitted when wETH is withdrawn from the contract, and a corresponding amount of wETH are burnt.
+     * @notice Emitted when wIMX is withdrawn from the contract, and a corresponding amount of wIMX are burnt.
      * @param account The address of the account that withdrew the tokens.
      * @param value The amount of tokens that were withdrawn.
      */
     event Withdrawal(address indexed account, uint256 value);
 
     /**
-     * @notice Deposit native ETH to the contract and mint an equal amount of wrapped ETH to msg.sender.
+     * @notice Deposit native IMX to the contract and mint an equal amount of wrapped IMX to msg.sender.
      */
     function deposit() external payable;
 
     /**
-     * @notice Withdraw given amount of native ETH to msg.sender after burning an equal amount of wrapped ETH.
+     * @notice Withdraw given amount of native IMX to msg.sender after burning an equal amount of wrapped IMX.
      * @param value The amount to withdraw.
      */
     function withdraw(uint256 value) external;

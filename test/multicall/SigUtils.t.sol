bb2fc43b3b5cfaf5b096e988ecbe7def7f50c806
Peter Robinson <drinkcoffee@eml.cc>
2025-06-10 17:02:53 +1000

    Migrate tests from HardHat to Forge

--- test/multicall/SigUtils.t.sol
+++ test/multicall/SigUtils.t.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// Copyright Immutable Pty Ltd 2018 - 2024
+// SPDX-License-Identifier: Apache 2.0
 pragma solidity >=0.8.19 <0.8.29;
 
 import {GuardedMulticaller2} from "../../contracts/multicall/GuardedMulticaller2.sol";
@@ -9,10 +10,16 @@ contract SigUtils {
 
     bytes32 internal constant CALL_TYPEHASH = keccak256("Call(address target,string functionSignature,bytes data)");
 
-    bytes32 internal constant MULTICALL_TYPEHASH =
+    bytes32 internal constant MULTICALL_TYPEHASHV1 =
+        keccak256(
+            "Multicall(bytes32 ref,address[] targets,bytes[] data,uint256 deadline)"
+        );
+
+    bytes32 internal constant MULTICALL_TYPEHASHV2 =
         keccak256(
             "Multicall(bytes32 ref,Call[] calls,uint256 deadline)Call(address target,string functionSignature,bytes data)"
         );
+
 
     bytes32 private immutable cachedDomainSeparator;
 
@@ -35,7 +42,31 @@ contract SigUtils {
         GuardedMulticaller2.Call[] calldata _calls,
         uint256 _deadline
     ) public view returns (bytes32) {
-        bytes32 digest = keccak256(abi.encode(MULTICALL_TYPEHASH, _reference, _hashCallArray(_calls), _deadline));
+        bytes32 digest = keccak256(abi.encode(MULTICALL_TYPEHASHV2, _reference, _hashCallArray(_calls), _deadline));
         return keccak256(abi.encodePacked("\x19\x01", cachedDomainSeparator, digest));
+    }
+
+
+    function hashTypedData(
+        bytes32 _reference,
+        address[] calldata _targets,
+        bytes[] calldata _data,
+        uint256 _deadline
+    ) public view returns (bytes32) {
+        bytes32 digest = keccak256(abi.encode(
+            MULTICALL_TYPEHASHV1, 
+            _reference, 
+            keccak256(abi.encodePacked(_targets)), 
+            hashBytesArray(_data), 
+            _deadline));
+        return keccak256(abi.encodePacked("\x19\x01", cachedDomainSeparator, digest));
+    }
+
+    function hashBytesArray(bytes[] memory _data) public pure returns (bytes32) {
+        bytes32[] memory hashedBytesArr = new bytes32[](_data.length);
+        for (uint256 i = 0; i < _data.length; i++) {
+            hashedBytesArr[i] = keccak256(_data[i]);
+        }
+        return keccak256(abi.encodePacked(hashedBytesArr));
     }
 }

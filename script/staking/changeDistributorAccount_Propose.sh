136ec0308bdd930e15ec4e02e5df2bddd2c4bf97
Peter Robinson <drinkcoffee@eml.cc>
2025-08-25 13:12:49 +1000

    Split out scripts for upgrade and changing distributor

--- script/staking/changeDistributorAccount_Propose.sh
+++ script/staking/changeDistributorAccount_Propose.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 FUNCTION_TO_EXECUTE='proposeChangeDistributor()'
-STAKEHOLDER_TYPE=WIMX
+STAKEHOLDER_TYPE=ANY
+script=script/staking/ChangeDistributor.t.sol:ChangeDistributor
 # Set-up variables and execute forge
 source $(dirname "$0")/common.sh
-
-

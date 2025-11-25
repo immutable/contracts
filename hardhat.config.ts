a0b28220822d4bd2d4e4c0a8fee84f2ff40c7724
Alex Connolly <25735635+alex-connolly@users.noreply.github.com>
2025-11-25 22:13:16 +1100

    v3

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,7 @@
-import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-viem";
+import "@nomicfoundation/hardhat-verify";
 
-import "@nomicfoundation/hardhat-foundry";
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "./deploy/x/asset";
-
-dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -67,6 +45,16 @@ const config: HardhatUserConfig = {
             "*": {
               "*": ["evm.assembly", "irOptimized", "devdoc"],
             },
+          },
+        },
+      },
+      {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
           },
         },
       },
@@ -130,10 +118,6 @@ const config: HardhatUserConfig = {
       url: process.env.MAINNET_URL || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * @imtbl/contracts
+ *
+ * Contract ABIs and deployed addresses for Immutable zkEVM
+ */
+
+// ABIs - for contracts you deploy
+export {
+  guardedMulticallerAbi,
+  guardedMulticaller2Abi,
+  immutableErc721Abi,
+  immutableErc721MintByIdAbi,
+  immutableErc1155Abi,
+  paymentSplitterAbi,
+} from "./generated";
+
+// Chain IDs and deployed addresses
+export {
+  CHAIN_ID,
+  IMMUTABLE_SIGNER,
+  STARTUP_WALLET_IMPL,
+  CHILD_ERC20_BRIDGE,
+  WALLET_FACTORY,
+  IMMUTABLE_SIGNED_ZONE,
+  IMMUTABLE_SIGNED_ZONE_V2,
+  SEAPORT_VALIDATOR,
+  IMMUTABLE_SEAPORT,
+  OPERATOR_ALLOWLIST,
+  CONTRACT_FACTORY,
+  IMMUTABLE_SWAP_PROXY,
+} from "./addresses";
+
+export type { ChainId } from "./addresses";

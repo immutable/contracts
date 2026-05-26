679f73e75823b5fc5830dd2a6dcccfdea9c71605
Peter Robinson <drinkcoffee@eml.cc>
2026-05-26 11:12:39 +1000

    Fix format

--- contracts/allowlist/OperatorAllowlistEnforcementErrors.sol
+++ contracts/allowlist/OperatorAllowlistEnforcementErrors.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Apache 2.0
 pragma solidity >=0.8.19 <0.8.29;
 
-
 interface OperatorAllowlistEnforcementErrors {
     /// @dev Error thrown when the operatorAllowlist address does not implement the IOperatorAllowlist interface
     error AllowlistDoesNotImplementIOperatorAllowlist();
